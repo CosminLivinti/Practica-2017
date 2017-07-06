b5a6f07f3509add1573dce6c58a93ccc0fbfc430
Livinti Cosmin <livinti.cosmin@yahoo.com>
2017-07-06 10:15:30 +0300

    Adaug Library.Test pentru Test Case-uri

--- Librarie/Library.Test/ScoreServiceTest.cs
+++ Librarie/Library.Test/ScoreServiceTest.cs
@@ -35,6 +35,24 @@ namespace Library.Test
         }
 
         [TestMethod]
+        public void ComputeScore_BookTypeSFBookScore2UserScore20_ShouldBe40()
+        {
+            //Arrange
+            var book = new Book()
+            {
+                Type = "SF",
+                Score = 2
+            };
+            const int userScore = 20;
+
+            //Act
+            int result = _scoreService.ComputeScore(book, userScore);
+
+            //Assert
+            Assert.AreEqual(40, result);
+        }
+
+        [TestMethod]
         public void ComputeScore_BookTypeSFBookScore2UserScore0_ShouldBe20()
         {
             //Arrange
@@ -53,7 +71,7 @@ namespace Library.Test
         }
 
         [TestMethod]
-        public void ComputeScore_BookkScore2UserScore1_ShouldBe3()
+        public void ComputeScore_BookScore2UserScore1_ShouldBe3()
         {
             //Arrange
             var book = new Book()
@@ -69,6 +87,43 @@ namespace Library.Test
             //Assert
             Assert.AreEqual(3, result);
         }
+
+        [TestMethod]
+        public void ComputeScore_BookTypeCopiiBookScore1UserScore5_ShouldBe7()
+        {
+            //Arrange
+            var book = new Book()
+            {
+                Type = "Copii",
+                Score = 1
+            };
+            const int userScore = 5;
+
+            //Act
+            int result = _scoreService.ComputeScore(book, userScore);
+
+            //Assert
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void ComputeScore_BookTypeCopiiBookScore2UserScore0_ShouldBe4()
+        {
+            //Arrange
+            var book = new Book()
+            {
+                Type = "Copii",
+                Score = 2
+            };
+            const int userScore = 0;
+
+            //Act
+            int result = _scoreService.ComputeScore(book, userScore);
+
+            //Assert
+            Assert.AreEqual(4, result);
+
+        }
     }
 
     internal class ScoreService : IScoreService
@@ -78,6 +133,13 @@ namespace Library.Test
             if(book.Type == "SF")
             {
                 return 10 * book.Score + userScore;
+            }
+            else
+            {
+                if (book.Type == "Copii")
+                {
+                    return 2 * book.Score + userScore;
+                }
             }
 
             return book.Score + userScore;
